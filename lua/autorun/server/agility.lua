7168e83c035ba1c800e534fda9969153282ce28d
Andrew Bass <andybass009@gmail.com>
2016-12-24 14:15:22 -0500

    Fix sliding, improve ledge detection

--- lua/autorun/server/agility.lua
+++ lua/autorun/server/agility.lua
@@ -1,12 +1,17 @@
 
 -- Determines how far or high a ledge can be in order to be grabbale by the player
 local ledgeReachUpwards = 40
-local ledgeReachOutwards = 10
+local ledgeReachOutwards = 15
+
+local ledgeHandGap = 20
 
 local releaseWaitTime = 0.2
 
-local jumpOffPower = 300
+local jumpOffPower = 250
 local wallJumpPower = 300
+
+local slidingSlowdown = 75
+local slidingSlopeBoost = 500
 
 local ledgeImpactSounds = {}
 for i = 1, 6 do
@@ -21,9 +26,9 @@ end
 local function LedgeTrace(ply)
     local camPos = ply:GetShootPos()
 
-    return util.TraceHull {
-        start = camPos + ply:GetUp() * ledgeReachUpwards,
-        endpos = camPos + ply:GetForward() * ledgeReachOutwards,
+    local leftHandTrace = util.TraceHull {
+        start = camPos + ply:GetUp() * ledgeReachUpwards - ply:GetRight() * ledgeHandGap * 0.2,
+        endpos = camPos + ply:GetForward() * ledgeReachOutwards - ply:GetRight() * ledgeHandGap,
 
         filter = ply,
 
@@ -32,9 +37,37 @@ local function LedgeTrace(ply)
 
         mask = MASK_ALL,
     }
+
+    local rightHandTrace = util.TraceHull {
+        start = camPos + ply:GetUp() * ledgeReachUpwards + ply:GetRight() * ledgeHandGap * 0.2,
+        endpos = camPos + ply:GetForward() * ledgeReachOutwards + ply:GetRight() * ledgeHandGap,
+
+        filter = ply,
+
+        mins = Vector(-16, -16, -16),
+        maxs = Vector(16, 16, 16),
+
+        mask = MASK_ALL,
+    }
+
+    if not leftHandTrace.Hit or not rightHandTrace.Hit then 
+        return { 
+            Hit = false
+        }
+    end
+
+    if leftHandTrace.StartSolid then
+        return rightHandTrace
+    elseif rightHandTrace.StartSolid then
+        return leftHandTrace
+    end
+
+    return leftHandTrace
 end
 
 local function IsLedgeDetected(ply, ledgeTrace)
+    if not ledgeTrace.Hit then return false end
+
     local roofTrace = util.TraceHull {
         start = ply:GetPos(),
         endpos = ply:GetPos() + ply:GetUp() * ledgeReachUpwards * 1.2,
@@ -80,9 +113,9 @@ local function AttachToLedge(ply, ledgeTrace)
     ply.GrabbedDist = (ledgeTrace.HitPos - ply:GetPos()).z
 
     if ply.GrabbedDist > 70 then
-        ply.VaultDelay = CurTime() + ply.GrabbedDist * 0.002
+        ply.TimeToCheckVault = CurTime() + ply.GrabbedDist * 0.002
     else
-        ply.VaultDelay = CurTime()
+        ply.TimeToCheckVault = CurTime()
     end
 
     ply:SetVelocity(-ply:GetVelocity())
@@ -90,6 +123,40 @@ local function AttachToLedge(ply, ledgeTrace)
 
     local sound = table.Random(ledgeImpactSounds)
     ply:EmitSound(sound)
+end
+
+local function VaultLedge(ply)
+    DeattachFromLedge(ply)
+
+    local vaultPower = 100 + math.max(ply.LedgeTrace.HitPos.z - ply.GrabbedPos.z, 0) * 2.5
+    local vaultDir = ply.VaultDir or 1
+
+    ply:SetVelocity(-ply:GetVelocity() + ply:GetUp() * vaultPower * 65)
+    ply:ViewPunch(Angle(vaultPower * 0.04, -vaultDir * vaultPower * 0.04, vaultDir * vaultPower * 0.04))
+
+    ply.VaultDir = -vaultDir
+
+    local sound = table.Random(ledgeVaultSounds)
+    ply:EmitSound(sound)
+end
+
+local function JumpOffLedge(ply)
+    DeattachFromLedge(ply)
+
+    local upwardsPower = 1 - math.max(ply:GetAimVector():Dot(ply:GetUp()), 0)
+
+    ply:SetVelocity(ply:GetAimVector() * jumpOffPower + ply:GetUp() * jumpOffPower * upwardsPower * 0.75)
+    ply:ViewPunch(Angle(-10, 0, 0))
+end
+
+local function LedgeTick(ply, moveData)
+    if ply.GrabbedWorld then 
+        moveData:SetOrigin(ply.GrabbedPos)
+        moveData:SetVelocity(Vector(0, 0, 0))
+    elseif ply.GrabbedLedge and ply.LedgeTrace.Entity then 
+        moveData:SetOrigin(ply.GrabbedRelativePos + ply.LedgeTrace.Entity:GetPos())
+        moveData:SetVelocity(ply.LedgeTrace.Entity:GetVelocity())
+    end
 end
 
 local function WallJumpTrace(ply, direction)
@@ -100,8 +167,8 @@ local function WallJumpTrace(ply, direction)
 
         filter = ply,
 
-        mins = ply:OBBMins() * 1.25,
-        maxs = ply:OBBMaxs() * 1.25,
+        mins = ply:OBBMins() * 1.5,
+        maxs = ply:OBBMaxs() * 1.5,
     }
 end
 
@@ -115,59 +182,66 @@ local function WallJump(ply, wallJumpTrace, direction)
     ply:EmitSound(sound, 100, 150)
 end
 
-local function VaultLedge(ply)
-    DeattachFromLedge(ply)
-
-    local vaultPower = 100 + math.max(ply.LedgeTrace.HitPos.z - ply.GrabbedPos.z, 0) * 2.5
-    local vaultDir = ply.VaultDir or 1
-
-    ply:SetVelocity(-ply:GetVelocity() + ply:GetUp() * vaultPower * 65)
-    ply:ViewPunch(Angle(vaultPower * 0.03, -vaultDir * vaultPower * 0.04, vaultDir * vaultPower * 0.03))
-
-    ply.VaultDir = -vaultDir
-
-    local sound = table.Random(ledgeVaultSounds)
-    ply:EmitSound(sound)
-end
-
--- TODO make camera look better
 local function StartSliding(ply)
     ply.IsSliding = true
-
-    ply.SlideTime = CurTime()
     ply.TimeToCheckSlide = CurTime() + 0.3
 
-    ply.SlideDir = ply:GetForward()
-    ply.RightSlideDir = ply:GetForward():Cross(ply:GetUp())
+    ply.SlideDir = ply:GetVelocity():GetNormalized()
+    ply.RightSlideDir = ply.SlideDir:Cross(ply:GetUp())
 
-    ply.SlideSpeed = ply:GetVelocity():Length() * 1.25
-
-    ply:ViewPunch(Angle(10, 0, 0))
+    ply.SlideSpeed = ply:GetVelocity():Length() * 1.5
+    ply:ViewPunch(Angle(-10, 0, 0))
 end
 
 local function StopSliding(ply)
     ply.IsSliding = false
+    ply.SlideVel = false
+    ply.TimeToCheckSlide = CurTime() + 0.3
 
-    ply:ViewPunch(Angle(0, 0, -10))
-    timer.Simple(0.3, function()
-        local eyeAngles = ply:EyeAngles()
-        eyeAngles.roll = 0
+    local eyes = ply:EyeAngles()
+    eyes.roll = 0
 
-        ply:SetEyeAngles(eyeAngles)
-    end)
+    ply:SetEyeAngles(eyes)
+end
+
+local function SlidingTick(ply, moveData)
+    -- Subtracts forward vector to simulate friction
+    local vel = ply.SlideVel or ply.SlideDir * ply.SlideSpeed
+
+    if ply:OnGround() then
+        local traceGround = util.TraceLine(util.GetPlayerTrace(ply, Vector(0, 0, -1)))
+        local velBoost = slidingSlopeBoost * traceGround.HitNormal:Dot(ply.SlideDir)
+
+        vel = vel + ply.SlideDir * velBoost * FrameTime() - ply.SlideDir * slidingSlowdown * FrameTime()
+    else 
+        ply.SlideTime = CurTime()
+    end
+
+    local forwardness = ply:GetForward():Dot(ply.SlideDir)
+    local rollDir = ply:GetForward():Dot(ply.RightSlideDir) > 0 and 1 or -1
+
+    local slideRoll = math.deg(math.acos(forwardness)) * 0.25 * rollDir
+
+    local curRoll = ply:EyeAngles().roll
+    local newAngle = Angle(ply:EyeAngles().pitch, ply:EyeAngles().yaw, Lerp(10 * FrameTime(), curRoll, slideRoll))
+
+    ply:SetEyeAngles(newAngle)
+    
+    -- If our speed gets low due to collision or friction, just stop sliding
+    if vel:GetNormalized():Dot(ply.SlideDir) < 0 then
+        moveData:SetVelocity(Vector())
+    else 
+        moveData:SetVelocity(vel)
+    end
+
+    ply.SlideVel = vel
 end
 
 local function PlayerTick(ply, moveData)
     if ply.GrabbedLedge then
-        if ply.GrabbedWorld then 
-            moveData:SetOrigin(ply.GrabbedPos)
-            moveData:SetVelocity(Vector(0, 0, 0))
-        elseif ply.GrabbedLedge and ply.LedgeTrace.Entity then 
-            moveData:SetOrigin(ply.GrabbedRelativePos + ply.LedgeTrace.Entity:GetPos())
-            moveData:SetVelocity(ply.LedgeTrace.Entity:GetVelocity())
-        end
+        LedgeTick(ply, moveData)
         
-        if ply:KeyDown(IN_SPEED) and ply:KeyDown(IN_FORWARD) and CurTime() > ply.VaultDelay then
+        if ply:KeyDown(IN_SPEED) and ply:KeyDown(IN_FORWARD) and CurTime() > ply.TimeToCheckVault then
             VaultLedge(ply)
         end
 
@@ -185,30 +259,15 @@ local function PlayerTick(ply, moveData)
             if CurTime() > ply.TimeToCheckSlide and (not ply:Crouching() or not ply:KeyDown(IN_DUCK)) then
                 StopSliding(ply)
             else 
-                moveData:SetVelocity(ply.SlideDir * ply.SlideSpeed - slowDown)
+                SlidingTick(ply, moveData)
             end
-
-            local forwardness = ply:GetForward():Dot(ply.SlideDir)
-            local rollDir = ply:GetForward():Dot(ply.RightSlideDir) > 0 and 1 or -1
-
-            local rollAngle = math.deg(math.acos(forwardness)) * 0.25 * rollDir
-            ply:SetEyeAngles(Angle(ply:EyeAngles().pitch, ply:EyeAngles().yaw, 0) + Angle(0, 0, rollAngle))
         end
     end
 end
 
-local function JumpOffLedge(ply)
-    DeattachFromLedge(ply)
-
-    local upwardsPower = 1 - math.max(ply:GetAimVector():Dot(ply:GetUp()), 0)
-
-    ply:SetVelocity(ply:GetAimVector() * jumpOffPower + ply:GetUp() * jumpOffPower * upwardsPower * 0.75)
-    ply:ViewPunch(Angle(-10, 0, 0))
-end
-
 hook.Add("PlayerTick", "agility_PlayerTick", PlayerTick)
 
-hook.Add("KeyPress", "agility_KegPress", function(ply, key)
+hook.Add("KeyPress", "agility_PlayerKeyPress", function(ply, key)
     if ply.GrabbedLedge then 
         if key == IN_DUCK then
             DeattachFromLedge(ply)
@@ -226,9 +285,11 @@ hook.Add("KeyPress", "agility_KegPress", function(ply, key)
         if wallJumpTrace.Hit then
             WallJump(ply, wallJumpTrace, direction)
         end
-    elseif ply:OnGround() and not ply:Crouching() and key == IN_DUCK and ply:KeyDown(IN_SPEED) and ply:KeyDown(IN_FORWARD) then 
+    elseif not ply.IsSliding and ply:OnGround() and not ply:Crouching() and key == IN_DUCK and ply:KeyDown(IN_SPEED) and ply:KeyDown(IN_FORWARD) then 
         StartSliding(ply)
     elseif ply.IsSliding and key == IN_JUMP then
         StopSliding(ply)
+    elseif key == IN_USE and ply:KeyDown(IN_SPEED) then
+        -- Shoot dodge
     end
 end)
